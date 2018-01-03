b5714cd9f9341e828365180051fbe386f0eb14ae
Anthony T. Brown <tony@Anthonys-MacBook-Pro.local>
2018-01-03 12:04:47 -0500

    removed pathogen and moved plugins to vim 8 package system

--- vim/configure.sh
+++ vim/configure.sh
@@ -1,30 +1,13 @@
 #!/bin/bash
 
 # install vimrc 
-rsync -avz --progress vimrc ~/.vimrc
-# install pathogen if not already installed
-if [ -f ~/.vim/autoload/pathogen.vim ]
-then ## do nothing ;
-	echo -e "pathogen.vim ... [\e[32:1mok\e[0m]";
-else ## install pathogen
-	if [ -d ~/.vim/autoload ];then # if dir exists do nothing
-		echo "autoload ....... [\e[32:1mok\e[0m]";
-	else # make dire and all its parents
-		mkdir -p ~/.vim/autoload;
-	fi
+cp vimrc ~/.vimrc
 
-	if [ -d ~/.vim/bundle ];then # if dir exists do nothing
-		echo "bundle ......... [\e[32:1mok\e[0m]";
-	else # make dire and all its parents
-		mkdir -p ~/.vim/bundle;
-	fi
-
-	curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pathogen.vim
-fi
+mkdir -p ~/.vim/pack/plugins
 
 # myPlugininstaller name  "repo" path
 function myPluginInstaller() {
-	if [ -d ~/.vim/bundle/$1 ]
+	if [ -d ~/.vim/pack/plugins/start/$1 ]
 	then # do nothing
 		echo -e "$1.vim ... [\e[32:1mok\e[0m]";
 	else # install node.vim
