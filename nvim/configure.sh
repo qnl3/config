61c2cc0a24cfc0fb1f1946caf52330bcdb72861c
qnl3 <brown.anthon.t@gmail.com>
2019-05-09 10:54:54 -0400

    updated nvim/configure.sh
    commented out python and go nvim
    This script now only installs vim-plug and the nvim config file

--- nvim/configure.sh
+++ nvim/configure.sh
@@ -1,35 +1,36 @@
 #!/bin/bash
 
 ConfigFolder="$HOME/.config/nvim"
-DataFolder="$HOME/.local/share/nvim/site"
+DataFolder="$HOME/.local/share/nvim"
 ConfigFile="$ConfigFolder/init.vim"
 
 mkdir -p $ConfigFolder
 mkdir -p $DataFolder
 
-PluginFolder="/nvim/site/plug"
+PluginFolder="$DataFolder/plug"
+VIMPlug="$DataFolder/site/autoload/plug.vim"
 
 # install configuration file 
 cp -f init.vim $ConfigFile
 
 
 ## install vim-plug
-if [ ! -f $DataFolder/autoload/plug.vim ];then
+if [ ! -f $VIMPlug ];then
 	echo "[installing] vim-plug - plugin manager"
-	curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+	curl -fLo $VIMPlug --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 else
 	echo "[skiped] vim-plug - already installed"
 fi
 
-echo "*** configure neovim python plugin ***"
-pip3 show neovim 2&> /dev/null
-if [ $? -ne 0 ];then
-	echo "[installing] neovim python plugin - already installed"
-	pip install neovim
-fi
+#echo "*** configure neovim python plugin ***"
+#pip3 show neovim 2&> /dev/null
+#if [ $? -ne 0 ];then
+#	echo "[installing] neovim python plugin - already installed"
+#	pip install neovim
+#fi
 
 # make sure gocode is installed
-go get -u github.com/nsf/gocode
+#go get -u github.com/nsf/gocode
 	
 ## install surround.vim
 #myPluginInstaller surround "https://github.com/tpope/vim-surround.git" surround
